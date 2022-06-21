293d52172f527a993651a7947f96babb9b53048d
Riddhiman Goswami <buddhadebgoswami.de@outlook.com>
2022-06-21 13:51:59 +0530

    js writing

--- Java-Script/course.js
+++ Java-Script/course.js
@@ -1,2 +1,35 @@
-var myStr = "This is the start. " + "\nThis is the end"
-console.log(myStr);
+console.log("hello from javascript")
+var number = 5; // in-line comment
+/* this is a 
+multi line comment */
+/* Data types:
+undefined, null, boolean, string, symbol, number and object
+*/
+var myName = "Riddhiman"
+console.log(myName)
+myName = 13
+console.log(myName)
+let ourName = "Cooder"
+const pi = 3.14
+console.log(ourName)
+console.log(pi)
+//changing variables
+var a;
+var b = 2;
+console.log(a)
+console.log(b)
+a=7;
+b=a;
+console.log(a)
+console.log(b)
+//Iniatialize these 3 variables
+var a = 5;
+var b = 10;
+var c = "I am a ";
+//Do not change code below this line
+a = a + 5;
+b = b + 10;
+c = c + "String!";
+console.log(a)
+console.log(b)
+console.log(c)
