e03009674bc8130df3ac0eac97f54f8b4cb85882
Riddhiman Goswami <buddhadebgoswami.de@outlook.com>
2022-06-22 14:19:33 +0530

    till adding lines

--- Java-Script/course.js
+++ Java-Script/course.js
@@ -33,3 +33,78 @@ c = c + "String!";
 console.log(a)
 console.log(b)
 console.log(c)
+var sum = 10 + 10;
+console.log(sum)
+var difference = 45 - 33;
+console.log(difference)
+var product = 8 * 10;
+console.log(product)
+var quotient = 66 /10
+console.log(quotient)
+var myVar = 87;
+myVar ++;
+console.log(myVar)
+var myVar = 11;
+myVar --;
+console.log(myVar)
+var remainder;
+remainder = 11 % 3
+console.log(remainder)
+var a = 3;
+var b = 17;
+var c = 12;
+//Only modify code below this line
+a += 12;
+b += 9;
+c += 7;
+console.log(a)
+console.log(b)
+console.log(c)
+var a = 11;
+var b = 9;
+var c = 3;
+//Only modify code below this line
+a -= 6;
+b -= 15;
+c -= 1;
+console.log(a)
+console.log(b)
+console.log(c)
+var a = 5;
+var b = 12;
+var c = 4.6;
+//Only modify code below this line
+a *= 5;
+b *= 3;
+c *= 10;
+console.log(a)
+console.log(b)
+console.log(c)
+var a = 48;
+var b = 108;
+var c = 33;
+//Only modify code below this line
+a /= 12;
+b /= 4;
+c /= 11;
+console.log(a)
+console.log(b)
+console.log(c)
+var myStr = "I am a \"double quoted\" string inside \"double quotes\"";
+console.log(myStr)
+var myStr = '"I am a "double quoted" string inside "single quotes"';
+console.log(myStr)
+/*****
+ * \' single quote
+ * \" double quote
+ * \\ backslash
+ * \n new line
+ * \r carriage return
+ * \t tab
+ * \b backspace
+ * \f formfeed
+ *****/
+var myStr = "first line\n\t\\Second line\nthird line";
+console.log(myStr)
+var ourStr = "I came first" + " I came second";
+console.log(ourStr)
