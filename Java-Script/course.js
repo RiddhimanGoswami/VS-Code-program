c8a78482efa65a30f110a3e538e67faf1f6f6846
Riddhiman Goswami <buddhadebgoswami.de@outlook.com>
2022-06-25 15:12:28 +0530

    140 line

--- Java-Script/course.js
+++ Java-Script/course.js
@@ -128,3 +128,13 @@ var firstletteroffirstname = "";
 var firstname = "Riddhiman"
 firstletteroffirstname = firstname[0]
 console.log(firstletteroffirstname)
+//Example
+var firstname = "Riddhi";
+var secondletter = firstname[1];
+var thirdletter = firstname[2];
+console.log(secondletter);
+console.log(thirdletter);
+//Example
+var firstname = "Riddhi";
+var lastletteroffirstname = firstname[firstname.length - 1]
+console.log(lastletteroffirstname);
