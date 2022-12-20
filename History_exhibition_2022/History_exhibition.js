c25236483ce6a0e6e56c8f9fac042f2e6e4305de
Riddhiman Goswami <buddhadebgoswami.de@outlook.com>
2022-12-20 12:30:10 +0530

    hist

---
+++ History_exhibition_2022/History_exhibition.js
@@ -0,0 +1,15 @@
+function click(){
+    var hero = document.getElementById("hero");
+    hero.innerHTML = window.location.href = "http://127.0.0.1:5500/History_exhibition_2022/History_exhibition_non_co-operation.html"
+}
+document.getElementById("btn1").addEventListener("click",click);
+function click(){
+    var hero = document.getElementById("hero");
+    hero.innerHTML = window.location.href = "http://127.0.0.1:5500/History_exhibition_2022/Feedback_history.html"
+}
+document.getElementById("btn2").addEventListener("click",click);
+function click(){
+    var hero = document.getElementById("hero");
+    hero.innerHTML = window.location.href = "http://127.0.0.1:5500/History_exhibition_2022/acknowledgement.html"
+}
+document.getElementById("btn3").addEventListener("click",click);
